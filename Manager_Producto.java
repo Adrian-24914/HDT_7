bb00bbcc5b0f32574d3f9ba555c2334838d94bbe
Adrian-24914 <pen24914@uvg.edu.gt>
2025-03-25 22:49:07 -0600

    Prevención de error si uno de los datos del csv se encuentra vacío

--- Manager_Producto.java
+++ Manager_Producto.java
@@ -1,18 +1,29 @@
 import java.io.*;
-import java.util.*;
 
 public class Manager_Producto {
     private BinarySearchTree<Producto> bst = new BinarySearchTree<>();
 
+    // Carga productos desde un archivo CSV
     public void loadProducts(String fileName) {
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             br.readLine(); // Omitimos encabezado
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(",");
+                
+                // Asegurarse de que las columnas de precio no estén vacías
                 String sku = data[6];
-                double priceRetail = Double.parseDouble(data[9]);
-                double priceCurrent = Double.parseDouble(data[10]);
+                
+                double priceRetail = 0.0;
+                if (!data[9].isEmpty()) {
+                    priceRetail = Double.parseDouble(data[9]);
+                }
+                
+                double priceCurrent = 0.0;
+                if (!data[10].isEmpty()) {
+                    priceCurrent = Double.parseDouble(data[10]);
+                }
+                
                 String productName = data[18];
                 String category = data[0];
 
@@ -24,6 +35,7 @@ public class Manager_Producto {
         }
     }
 
+    // Buscar un producto por su SKU
     public Producto searchProduct(String sku) {
         return bst.search(new Producto(sku, 0, 0, "", ""));
     }
